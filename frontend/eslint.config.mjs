1929a86b44e40a40880d3d708934ad0deef39592
talaf <artemk2504@gmail.com>
2025-11-29 20:09:11 +0000

    Keep calm and commit

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -13,6 +13,11 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    rules: {
+      "@typescript-eslint/no-require-imports": "off",
+    },
+  },
 ]);
 
 export default eslintConfig;
