150b8120aa44f4587a191ce8d1e14926515b3739
talaf <artemk2504@gmail.com>
2025-12-04 23:32:20 +0000

    feat:init

--- backend/src/twilio/twilio.controller.ts
+++ backend/src/twilio/twilio.controller.ts
@@ -1,10 +1,10 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { TwilioService } from './twilio.service';
 import { SendVerifyCodeDto } from './dto/send-verify-code.dto';
 
 @Controller('twilio')
 export class TwilioController {
-  constructor(private readonly twilioService: TwilioService) {}
+  constructor(private readonly twilioService: TwilioService) { }
   @Post('')
   async sendVerifyCode(@Body() data: SendVerifyCodeDto) {
     return this.twilioService.snedVerifyCode(data);
