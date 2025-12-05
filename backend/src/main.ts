281b9662e71f39084fb6d41e017f9b963186175f
talaf <artemk2504@gmail.com>
2025-12-05 09:52:24 +0000

    feat:sheets

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['https://farael-frontend.esp.ovh', 'http://localhost:3000'],
+    origin: true,
   });
   app.useGlobalPipes(
     new ValidationPipe({
