2406f0a1b30c5766b0419feab145d0f6d15aecbe
talaf <artemk2504@gmail.com>
2025-12-08 13:00:48 +0000

    feat:send lead

--- frontend/entities/step/whats-app/ui/whats-app-icon/index.tsx
+++ frontend/entities/step/whats-app/ui/whats-app-icon/index.tsx
@@ -1,16 +1,22 @@
 import Image from "next/image";
-import Link from "next/link";
 import whatsAppIcon from "@/public/Group 5.svg";
 import { useSendPixel } from "@/features/facebook-pixel";
 import { PixelEvent } from "@/features/facebook-pixel/enums/events.enum";
 export const WhatsAppIcon = () => {
   const { handlerSendPixel } = useSendPixel();
+  const handleClick = () => {
+    handlerSendPixel(PixelEvent.Lead);
+
+    setTimeout(() => {
+      window.location.href = "https://www.whatsapp.com/?lang=en";
+    }, 150);
+  };
   return (
-    <Link
-      onClick={() => handlerSendPixel(PixelEvent.Lead)}
-      href="https://www.whatsapp.com/?lang=en"
+    <div
+      onClick={handleClick}
+      style={{ cursor: "pointer", display: "inline-block" }}
     >
-      <Image src={whatsAppIcon} alt="" />
-    </Link>
+      <Image src={whatsAppIcon} alt="WhatsApp" />
+    </div>
   );
 };
