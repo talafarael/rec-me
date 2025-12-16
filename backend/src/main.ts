98b120d4a3739d5bcecfd0cbe20e144792009134
talaf <artemk2504@gmail.com>
2025-12-16 18:11:19 +0000

    feat:docker backen

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,12 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['https://farael-frontend.esp.ovh', 'http://localhost:3000'],
+    origin: [
+      'https://farael-frontend.esp.ovh',
+      'http://localhost:3000',
+      'https://lead-form.website',
+      'http://lead-form.website',
+    ],
     methods: 'GET,POST,PUT,PATCH,DELETE,OPTIONS',
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization', 'x-waf-captcha-token'],
