af8700132a79d9dcc0fad5d59c7200004b52ff8d
talaf <artemk2504@gmail.com>
2025-12-16 11:54:16 +0000

    feat:docker backen

--- backend/src/config/configuration.ts
+++ backend/src/config/configuration.ts
@@ -1,17 +1,8 @@
 export default () => ({
-  sendpulse: {
-    secret: process.env.SENDPULSE_SECRET,
-    clientId: process.env.SENDPULSE_ID,
-    addressBookId: process.env.SENDPULSE_ADDRESS_BOOK_ID,
-  },
   twilio: {
     accountsid: process.env.TWILIO_ACCOUNTSID,
     authtoken: process.env.TWILIO_AUTHTOKEN,
     phone: process.env.TWILIO_PHONE,
-  },
-  telegram: {
-    token: process.env.TELEGRAM_TOKEN,
-    chatId: process.env.TELEGRAM_CHAT_ID,
   },
   admin: {
     password: process.env.ADMIN_PASSWORD,
